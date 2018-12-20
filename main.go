f3815726650623c072072977f08ef33c82ec5cce
El-Kousy <12659090+elkousy@users.noreply.github.com>
2018-12-20 12:01:13 +0100

    refactor the implementation of graceful shutdown

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
@@ -24,11 +25,11 @@ func main() {
 	var err error
 
 	// handle syscall signals for a gracefull shutdown
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGQUIT)
-		errc <- fmt.Errorf("%s", <-c)
-	}()
+	//go func() {
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGQUIT)
+	//errc <- fmt.Errorf("%s", <-c)
+	//}()
 
 	// init database
 	db, err := payments.DbConnect()
@@ -52,8 +53,15 @@ func main() {
 	endpoints := payments.MakeEndpoints(svc)
 
 	// Instances a new HTTP server
+
+	httpAddr := ":" + strconv.Itoa(config.AppPort)
+	s := &http.Server{
+		Addr:         httpAddr,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
 	go func() {
-		httpAddr := ":" + strconv.Itoa(config.AppPort)
 		mux := mux.NewRouter()
 
 		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -66,13 +74,7 @@ func main() {
 
 		logger.LogStdOut.Info(fmt.Sprintf("The %s has started on port %s", config.AppName, httpAddr))
 
-		s := &http.Server{
-			Addr:         httpAddr,
-			ReadTimeout:  5 * time.Second,
-			WriteTimeout: 10 * time.Second,
-			Handler:      mux,
-		}
-
+		s.Handler = mux
 		errc <- s.ListenAndServe()
 	}()
 
@@ -80,7 +82,6 @@ func main() {
 	go func() {
 		opsHTTPAddr := ":" + strconv.Itoa(config.OpsPort)
 		mux := mux.NewRouter()
-
 		mux.Handle("/metrics", promhttp.Handler())
 		mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
@@ -89,7 +90,6 @@ func main() {
 		errc <- http.ListenAndServe(opsHTTPAddr, mux)
 	}()
 
-	
 	// launch a dedicated webserver for pprof
 	go func() {
 		debugHTTPAddr := ":" + strconv.Itoa(config.DebugPort)
@@ -98,7 +98,26 @@ func main() {
 		http.ListenAndServe(debugHTTPAddr, nil)
 	}()
 
-	fmt.Println("exit", <-errc)
+	// Block waiting for a receive on either channel
+	select {
+	case err := <-errc:
+		logger.LogStdErr.Fatalf("Error starting server: %v", err)
+	case <-osSignals:
+
+		// Create a context to attempt a graceful 5s shutdown
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		if err := s.Shutdown(ctx); err != nil {
+			logger.LogStdErr.Fatalf("Could not stop the http server gracefully: %v", err)
+			if err := s.Close(); err != nil {
+				logger.LogStdErr.Fatalf("Could not stop the http server: %v", err)
+			}
+		}
+	}
+
+	fmt.Println("Shutdown down successfull")
 }
 
 func getStackTraceHandler(w http.ResponseWriter, r *http.Request) {
